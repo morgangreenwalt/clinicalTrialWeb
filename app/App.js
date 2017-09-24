f3a5893e5d27695238f99201f22464adaeca8a10
Tin Nguyen <Tinman@Tins-MacBook-Pro.local>
2017-09-23 20:43:54 -0500

    setting up auth0

--- app/App.js
+++ app/App.js
@@ -4,4 +4,10 @@ import {render} from "react-dom";
 import Routes from "./config/routes";
 
 // Renders the contents according to the route page.
+
+import Auth from './config/Auth.js';
+
+const auth = new Auth();
+auth.login();
+
 render(<Routes />, document.getElementById("app"));
