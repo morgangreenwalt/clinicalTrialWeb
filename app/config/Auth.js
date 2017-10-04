9ec840a84b8cd0451bf504503063b5ec0101da4b
morgan greenwalt <you@example.com>
2017-10-04 17:30:49 -0500

    edits

--- app/config/Auth.js
+++ app/config/Auth.js
@@ -5,7 +5,7 @@ export default class Auth {
   auth0 = new auth0.WebAuth({
     domain: 'ttn10.auth0.com',
     clientID: 'VwwqAq8UXhy4ZhG8OoeFHeTTR61B4gsW',
-    redirectUri: 'http://localhost:3000/login',
+    redirectUri: 'https://enigmatic-bastion-52836.herokuapp.com/login',
     audience: 'https://ttn10.auth0.com/userinfo',
     responseType: 'token id_token',
     scope: 'openid profile'
